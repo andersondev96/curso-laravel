4db2a2f9dd6aad5e2b405797c1351049271acd78
AndersonUfop <49786548+AndersonUfop@users.noreply.github.com>
2020-04-01 17:45:32 -0300

    updates

---
+++ mail/app/Listeners/HomeEventListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\HomeEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class HomeEventListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    
+    public function __construct($text)
+    {
+        
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  HomeEvent  $event
+     * @return void
+     */
+    public function handle(HomeEvent $event)
+    {
+        info("Entrou no home");
+        info($event->text);
+    }
+}
